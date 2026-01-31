355117f972ac6e627c0d1d5fbb5db0a318001bd2
BasharatleoGill <basharatgill494@gmail.com>
2026-01-31 14:09:27 +0500

    Update MazeGeneratorPro.py

--- MazeGeneratorPro.py
+++ MazeGeneratorPro.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "Maze Generator Pro",
-    "author": "Your Name",
+    "author": "BasharatLeoGill",
     "version": (4, 6),
     "blender": (2, 80, 0),
     "location": "View3D > N-Panel > Maze Pro",
